2c90984ffdf1354331cb60d76baf5ca58b8a0045
Matthew Seremet <xskater@gmail.com>
2015-12-20 07:47:22 -0500

    initial commit

---
+++ preview/byebox.js
@@ -0,0 +1,56 @@
+
+removableByeContent = [];
+
+function byeBoxHideOverflow(){
+	$(removableByeContent[0]).closest('.bye-box-content-container').addClass('box-hide-overflow');
+	console.log('hide overflow?');
+}
+
+function removeByeContent(){
+		del = $(removableByeContent.shift());
+		console.log(removableByeContent);
+		del.closest('.bye-box-container').addClass('emptying');
+		$(window).trigger('by-box-emptying');
+		console.log(del);
+		del.closest('.bye-box-container').css({'min-width':"0px",'min-height':'0px','height':'','width':''}).addClass('emptied').removeClass('emptying');
+		del.closest('.bye-box-container').removeClass('bye-box-leave');
+		
+		console.log((del.is('[data-next-content]')));
+		if(typeof(del.attr('data-next-content')) === "undefined"){
+			del.addClass('empty').empty();
+			$(window).trigger('by-box-emptied');
+		}else{
+			console.log(del);
+			fillByeBox(
+			del.closest('.bye-box-container'),
+			del.attr('data-next-content')
+			);
+		}
+}
+
+function fillByeBox(selector, content){
+	console.log([selector, content]);
+	$(selector).find('.bye-box-content').html(content);
+	$(selector).trigger('byeboxfill');
+}
+
+$(document).on('byeboxfill','.bye-box-container', function(){
+	console.log($(this));
+	$(this).removeClass('emptied');
+	$(this).find('.bye-box-content').removeClass('empty');
+});
+
+$(document).on('click','.bye-button',function(e){
+	e.preventDefault();
+	removableByeContent.push($(this).closest('.bye-box-content'));
+	setTimeout(function(){ byeBoxHideOverflow() },550);
+	setTimeout(function(){ removeByeContent() },1000);
+	$(this).closest('.bye-box-container')
+		.css({
+			'min-height':function(){return $(this).innerHeight()},
+			'min-weight':function(){return $(this).innerWidth()},
+			'height':function(){return $(this).innerHeight()},
+			'weight':function(){return $(this).innerWidth()}
+			});
+	$(this).closest('.bye-box-container').addClass('bye-box-leave');
+});
